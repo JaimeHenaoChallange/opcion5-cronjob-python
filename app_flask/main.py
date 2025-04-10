ff2863288ecd57291e39cd2943588c26de79dd4e
jhenao-nex <jaime.andres.henao.arbelaez@ba.com>
2025-04-10 23:01:00 +0000

    feat: fix flask app

--- app_flask/main.py
+++ app_flask/main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World! Esta es una aplicación Flask desplegada con ArgoCD."
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    shape = None
+    size = 100
+    color = '#3498db'
+
+    if request.method == 'POST':
+        shape = request.form.get('shape')
+        size = int(request.form.get('size'))
+        color = request.form.get('color')
+
+    return render_template('index.html', shape=shape, size=size, color=color)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
